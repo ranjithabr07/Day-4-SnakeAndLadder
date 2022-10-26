8d118d5cb4ebd4b0c13208c500ebd67ec30db963
ranjithabr07 <ranjitharanjubrhlk1998@gmail.com>
2022-10-26 12:56:55 +0530

    Player reach winning position

--- SnakeAndLadderProblem/SnakeAndLadderProblem/SnakeAndLadder.cs
+++ SnakeAndLadderProblem/SnakeAndLadderProblem/SnakeAndLadder.cs
@@ -9,15 +9,50 @@ namespace SnakeAndLadderProblem
     internal class SnakeAndLadder
     {
         int playerPosition = 0;
+        const int NO_PLAY = 1;
+        const int LADEER = 2;
+        const int SNAKE = 3;
+        const int WINNING_POSITION = 100;
         public void StartGame()
         {
             Console.WriteLine("Game Started ");
             Console.WriteLine("Player Position:" + playerPosition);
-            Random random = new Random();
-            int dieRolled = random.Next(1, 7);
-            Console.WriteLine("Die Rolled: " + dieRolled);
-            playerPosition += dieRolled;
-            Console.WriteLine("Player position:" + playerPosition);
+
+            while (playerPosition < WINNING_POSITION)
+            {
+                Random random = new Random();
+                int dieRolled = random.Next(1, 7);
+                int option = random.Next(1, 4);
+
+                switch (option)
+                {
+                    case NO_PLAY:
+                        Console.WriteLine("No play");
+                        break;
+
+                    case LADEER:
+                        playerPosition += dieRolled;
+                        Console.WriteLine(" Dice Rolls Number : +{0} ", dieRolled);
+                        Console.WriteLine("Ladder: " + playerPosition);
+                        break;
+
+                    case SNAKE:
+                        playerPosition -= dieRolled;
+                        Console.WriteLine(" Dice Rolls Number : -{0} ", dieRolled);
+                        Console.WriteLine("Snake: " + playerPosition);
+                        if (playerPosition < 0)
+                        {
+                            playerPosition = 0;
+                            Console.WriteLine("Player restart from :" + playerPosition);
+                        }
+                        break;
+
+                }
+                if (playerPosition >= WINNING_POSITION)
+                {
+                    Console.WriteLine("Player Won the Game");
+                }
+            }
         }
     }
 }
